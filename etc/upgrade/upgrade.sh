eb49f134ae4a8a943da376112a33f62c428f7f0e
Shuhei Kinukawa <kinushu@gmail.com>
2025-06-29 22:44:09 +0900

    [fix] asdf不要なため対応

--- etc/upgrade/upgrade.sh
+++ etc/upgrade/upgrade.sh
@@ -6,12 +6,12 @@ source $HOME/.zshrc
 
 set -eu
 
-## ruby, rbenv
-cd ~/.rbenv
-git pull
+# ## ruby, rbenv
+# cd ~/.rbenv
+# git pull
 
-cd ~/.rbenv/plugins/ruby-build
-git pull
+# cd ~/.rbenv/plugins/ruby-build
+# git pull
 
 #cd ~/.pyenv
 #git pull
@@ -24,15 +24,18 @@ set -eu
 
 brew upgrade
 
+## mise で latest をinstallする。
+mise install
+
 ## asdf
-asdf install python latest
-asdf set -u python latest
+# asdf install python latest
+# asdf set -u python latest
 
-asdf install golang latest
-asdf set -u golang latest
+# asdf install golang latest
+# asdf set -u golang latest
 
-asdf install nodejs latest
-asdf set -u nodejs latest
+# asdf install nodejs latest
+# asdf set -u nodejs latest
 
 brew doctor
 
